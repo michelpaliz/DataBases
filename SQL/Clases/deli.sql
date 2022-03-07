cff0ac3065d7ce4de573167c34efd5bbd6449614
michelpaliz <76707533+michelpaliz@users.noreply.github.com>
2022-03-07 20:58:47 +0100

    changes

--- SQL/Clases/deli.sql
+++ SQL/Clases/deli.sql
@@ -191,3 +191,191 @@ FROM
   ) mitabla
 GROUP BY
   ejer;
+-- *Fecha 7/3/2022
+Select
+  *
+from
+  llx_product;
+Select
+  *
+from
+  llx_c_tva;
+Select
+  *
+from
+  llx_c_tva
+where
+  taux in(21, 10, 4);
+Select
+  *
+from
+  llx_c_tva
+where
+  note like "%IVA%";
+select
+  *
+from
+  llx_c_country;
+-- Sacar todos los tipos de iva y contador cuantos productos hay por cada iva, sabiendo que el codigo del pais es ESP
+SELECT
+  IVA.*
+from
+  llx_c_country Pais
+  JOIN llx_c_tva IVA ON Pais.rowid = Iva.fk_pays
+WHERE
+  Pais.code = "ES";
+-- Ahora solo saca los tipos de ivas
+SELECT
+  IVA.Taux iva
+from
+  llx_c_country Pais
+  JOIN llx_c_tva IVA ON Pais.rowid = Iva.fk_pays
+WHERE
+  Pais.code = "ES";
+-- que campos hay en la tabla productos que enlazan con el tipo de iva
+SELECT
+  impuestos.iva,
+  COUNT(producto.rowid)
+FROM
+  (
+    SELECT
+      IVA.taux iva
+    FROM
+      llx_c_country Pais
+      JOIN llx_c_tva IVA ON Pais.rowid = IVA.fk_pays
+    WHERE
+      Pais.code = "Es"
+  ) impuestos
+  JOIN llx_product producto ON impuestos.iva = producto.tva_tx
+GROUP BY
+  impuestos.iva;
+-- quiero que me saques todos los tipos de iva
+SELECT
+  impuestos.iva,
+  COUNT(producto.rowid)
+FROM
+  (
+    SELECT
+      IVA.taux iva
+    FROM
+      llx_c_country Pais
+      JOIN llx_c_tva IVA ON Pais.rowid = IVA.fk_pays
+    WHERE
+      Pais.code = "Es"
+  ) impuestos
+  left jOIN llx_product producto ON impuestos.iva = producto.tva_tx
+GROUP BY
+  impuestos.iva;
+-- saca un listado de todos los prov y cuenta el num de facturas de compras y de ventas que hemos echo con cada uno de ellos
+SELECT
+  *
+FROM
+  llx_societe;
+Select
+  *
+FROM
+  llx_facture;
+SELECT
+  v.rowid,
+  "venta",
+  v.fk_soc
+FROM
+  llx_facture v
+UNION
+SELECT
+  c.rowid,
+  "compra",
+  c.fk_soc
+FROM
+  llx_facture_fourn c -- segundo paso
+SELECT
+  COUNT(*)
+FROM
+  (
+    SELECT
+      v.rowid,
+      "venta" as tipo,
+      v.fk_soc
+    FROM
+      llx_facture v
+    UNION
+    SELECT
+      c.rowid,
+      "compra" AS tipo,
+      c.fk_soc
+    FROM
+      llx_facture_fourn c
+  ) CompraVenta;
+--Ultimo paso
+SELECT
+  sociedad.nom,
+  COUNT(compraventa.rowid)
+FROM
+  (
+    SELECT
+      v.rowid,
+      "venta" AS tipo,
+      v.fk_soc
+    FROM
+      llx_facture v
+    UNION
+    SELECT
+      c.rowid,
+      "compra" AS tipo,
+      c.fk_soc
+    FROM
+      llx_facture_fourn c
+  ) compraventa
+  RIGHT JOIN llx_societe sociedad ON compraventa.fk_soc = sociedad.rowid
+GROUP BY
+  compraventa.fk_soc;
+--ahora dividir las compras y con las ventas
+SELECT
+  sociedad.nom,
+  compraventa.tipo,
+  COUNT(compraventa.rowid)
+FROM
+  (
+    SELECT
+      v.rowid,
+      "venta" AS tipo,
+      v.fk_soc
+    FROM
+      llx_facture v
+    UNION
+    SELECT
+      c.rowid,
+      "compra" AS tipo,
+      c.fk_soc
+    FROM
+      llx_facture_fourn c
+  ) compraventa
+  RIGHT JOIN llx_societe sociedad ON compraventa.fk_soc = sociedad.rowid
+GROUP BY
+  compraventa.fk_soc;
+--quiero que aparezcan el total de cada factura
+SELECT
+  sociedad.nom,
+  compraventa.tipo,
+  COUNT(compraventa.rowid)
+FROM
+  (
+    SELECT
+      v.rowid,
+      "venta" AS tipo,
+      v.fk_soc
+    FROM
+      llx_facture v
+    UNION
+    SELECT
+      c.rowid,
+      "compra" AS tipo,
+      c.fk_soc
+    FROM
+      llx_facture_fourn c
+  ) compraventa
+  RIGHT JOIN llx_societe sociedad ON compraventa.fk_soc = sociedad.rowid
+GROUP BY
+  compraventa.fk_soc,
+  compraventa.tipo WITH ROLLUP;
+-- cuentamelo que tenga mayor de 3000 se utilizara el having que es el where de los group by
